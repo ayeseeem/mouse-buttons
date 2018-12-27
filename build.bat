e0f1dde385f5cb290230d1b5e117c02b33bfbdcf
ayeseeem <ayeseeem@gmail.com>
2018-12-27 10:50:05 +0000

    Initial version of mouse button swapper
    Code inspired by
    http://stackoverflow.com/questions/4806575/how-do-i-use-rundll32-to-SwapMouseButton
    Particularly
    https://stackoverflow.com/a/4806576
    and
    https://stackoverflow.com/a/10509038

---
+++ build.bat
@@ -0,0 +1,1 @@
+"%SystemRoot%\Microsoft.NET\Framework\v3.5\csc" swap.cs
---
+++ swap.cs
@@ -0,0 +1,51 @@
+// ayeseeem@gmail.com 2011-11-08
+using System.Runtime.InteropServices;
+using System;
+using System.Windows.Forms;
+
+class SwapMouse
+{
+    [DllImport("user32.dll")]
+    public static extern Int32 SwapMouseButton(Int32 bSwap);
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0) 
+        {
+            // try to toggle
+            bool swapped = SystemInformation.MouseButtonsSwapped;
+            if (swapped)
+            {
+                SetButtonsNormal();
+            }
+            else
+            {
+                SetButtonsSwapped();
+            }
+        }
+        else if (args.Length > 0 && String.Compare(args[0], "/R", true) == 0)
+            // set for right-handed
+            SetButtonsNormal();
+        else if (args.Length > 0 && String.Compare(args[0], "/L", true) == 0)
+            // set for left-handed
+            SetButtonsSwapped();
+        else
+        {
+            // Help
+            Console.WriteLine("swaps the mouse buttons");
+            Console.WriteLine(" no args - toggles left and right");
+            Console.WriteLine(" /R - set for normal Right-handed use");
+            Console.WriteLine(" /L - set for normal Left-handed use");
+        }
+    }
+
+    static void SetButtonsNormal()
+    {
+        SwapMouseButton(0);
+    }
+
+    static void SetButtonsSwapped()
+    {
+        SwapMouseButton(1);
+    }
+}
